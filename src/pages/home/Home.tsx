b5092c5bc9f4217668c7fbd9a3c169ccefc9cd2c
Stilllee <orodae@gmail.com>
2024-02-26 21:28:37 +0900

    ✨ 홈 컴포넌트 작업

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,17 +4,45 @@ export default function Home() {
   return (
     <>
       <header>
-        <div>
+        <nav>
           <Link to="/posts/new">글쓰기</Link>
           <Link to="/posts">게시글</Link>
           <Link to="/profile">프로필</Link>
-        </div>
+        </nav>
       </header>
-      <div className="post-list">Post List</div>
+      <nav className="post__navigation">
+        <div className="post__navigation-active">전체</div>
+        <div>나의 글</div>
+      </nav>
+      <section className="post__list">
+        {[...Array(10)].map((e, index) => (
+          <article key={index} className="post__box">
+            <Link to={`/posts/${index}`}>
+              <div className="post__profile-box">
+                <div className="post__profile" />
+                <div className="post__author-name">계정이름</div>
+                <div className="post__date">2024.02.26</div>
+              </div>
+              <div className="post__title">게시글 {index}</div>
+              <div className="post__text">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Obcaecati sint porro dolore dolor labore voluptatum dignissimos
+                adipisci magni repellat recusandae illum, nulla odio
+                exercitationem, consequatur explicabo ad, repudiandae molestiae
+                facere.
+              </div>
+              <div className="post__utils-box">
+                <div className="post__delete">삭제</div>
+                <div className="post__edit">수정</div>
+              </div>
+            </Link>
+          </article>
+        ))}
+      </section>
       <footer>
-        <div>Menu 1</div>
-        <div>Menu 2</div>
-        <div>Menu 3</div>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
       </footer>
     </>
   );
