9695f4063d2c88ea055ce09f44fd7220fd1cc8b1
Stilllee <orodae@gmail.com>
2024-03-04 21:58:08 +0900

    ✨ 다크모드 - toggle 기능 구현

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
+import { IoMdMoon } from "react-icons/io";
+import { IoSunnyOutline } from "react-icons/io5";
+import { useContext } from "react";
+import ThemeContext from "context/ThemeContext";
 
 export default function Footer() {
+  const context = useContext(ThemeContext);
+  console.log(context);
   return (
     <footer>
       <Link to="/posts/new">글쓰기</Link>
       <Link to="/posts">게시글</Link>
       <Link to="/profile">프로필</Link>
+      <button onClick={context.toggleMode} className="footer__theme-btn">
+        {context.theme === "light" ? <IoMdMoon /> : <IoSunnyOutline />}
+      </button>
     </footer>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 import { AuthContextProvider } from "context/AuthContext";
+import { ThemecontextProvider } from "context/ThemeContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <AuthContextProvider>
-    <Router>
-      <App />
-    </Router>
-  </AuthContextProvider>
+  <ThemecontextProvider>
+    <AuthContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthContextProvider>
+  </ThemecontextProvider>
 );
